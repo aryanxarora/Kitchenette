1aed02a1f5db8e44032b360cf3d10a18c361a4f8
Aryan Arora <aryanxarora@gmail.com>
2020-09-19 17:34:36 +0800

    views

---
+++ views/app.js
@@ -0,0 +1,39 @@
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+function smart_suggestions () {
+  document.getElementById("smartSuggestions").style.display = "block";
+  document.getElementById("recipes").style.display = "none";
+  document.getElementById("ingredients").style.display = "none";
+}
+
+function recipes () {
+  document.getElementById("smartSuggestions").style.display = "none";
+  document.getElementById("recipes").style.display = "block"
+  document.getElementById("ingredients").style.display = "none";
+}
+
+function ingredients () {
+  document.getElementById("smartSuggestions").style.display = "none";
+  document.getElementById("recipes").style.display = "none"
+  document.getElementById("ingredients").style.display = "block";
+}
+
+function addRecipe() {
+  alert("Adding new item")
+}
