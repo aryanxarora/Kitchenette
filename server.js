af853c6b75b61044cc09b0cc7ae2a9ab14791c7d
Aryan Arora <aryanxarora@gmail.com>
2020-09-26 13:06:35 +0800

    deployment setup

--- server.js
+++ server.js
@@ -45,10 +45,7 @@ const ingredients = [
     {ing_name: 'Pepper', ing_stock: false}
 ]
 
-
-app.listen(3000, () => {
-    console.log("Listening to port 3000...");
-})
+app.listen(process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
     if(req.session.loggedIn == true){
